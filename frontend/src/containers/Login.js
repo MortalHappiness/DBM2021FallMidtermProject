8f274ab00a29da220cd9430868bc6dd21f64c1c0
edisonhello <edisonhello@hotmail.com>
2021-11-01 13:29:43 +0800

    Listen on enter in login page

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -2,13 +2,22 @@
 import { Button, Input } from "@mui/material";
 import { useState } from "react";
 
-function Login({ user, previousUser, login }) {
+function Login({ user, previousUser, login: _login }) {
   const [ username, setUsername ] = useState(previousUser);
 
+  const login = () => {
+    if (username === "") return;
+    _login(username);
+  }
+
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') login();
+  }
+
   return (
-    <div>
+    <div onKeyDown={handleKeyDown}>
       <Input placeholder="username" defaultValue={username} onChange={e => setUsername(e.target.value)} />
-      <Button variant="contained" onClick={e => login(username)}>Login</Button>
+      <Button variant="contained" onClick={login}>Login</Button>
     </div>
   );
 }
