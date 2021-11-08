4d05eeba38d553f2bc2ece7542d22782c8e9926a
MortalHappiness <b07901069@ntu.edu.tw>
2021-11-08 23:58:20 +0800

    Password hash

--- backend/src/resolvers/MutationResolver.ts
+++ backend/src/resolvers/MutationResolver.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import jwt = require("jsonwebtoken");
+import bcrypt = require("bcrypt");
 import { Ctx, Resolver, Mutation, Arg } from "type-graphql";
 import { Context } from "../interfaces/context";
 
@@ -8,7 +9,7 @@ class MutationResolver {
   @Mutation((returns) => String)
   async login(
     @Arg("username") username: string,
-    @Arg("password") password: String,
+    @Arg("password") password: string,
     @Ctx() context: Context
   ) {
     const { prisma, secret } = context;
@@ -16,7 +17,8 @@ class MutationResolver {
     if (user === null) {
       throw new Error("Username not found");
     }
-    if (user.passwordHash !== password) {
+    const match = await bcrypt.compare(password, user.passwordHash);
+    if (!match) {
       throw new Error("Incorrect password");
     }
     return jwt.sign({ client_id: user.id }, secret, {
@@ -24,6 +26,25 @@ class MutationResolver {
       subject: user.id.toString(),
       expiresIn: "1h",
     });
+  }
+
+  @Mutation((returns) => String)
+  async register(
+    @Arg("username") username: string,
+    @Arg("password") password: string,
+    @Ctx() context: Context
+  ) {
+    const { prisma } = context;
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (user !== null) {
+      throw new Error("Username has already exists");
+    }
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    await prisma.user.create({
+      data: { username, passwordHash, displayName: username },
+    });
+    return username;
   }
 }
 
--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,52 +1,7 @@
 import "reflect-metadata";
-import { PubSubEngine } from "graphql-subscriptions";
-import {
-  Ctx,
-  Mutation,
-  Int,
-  Resolver,
-  Arg,
-  InputType,
-  Field,
-  Subscription,
-  Root,
-  ObjectType,
-  PubSub,
-  Query,
-  Authorized,
-} from "type-graphql";
+import { Ctx, Resolver, Query, Authorized } from "type-graphql";
 import { User } from "@generated/type-graphql";
 import { Context } from "../interfaces/context";
-import MutationType from "./MutationType";
-import SubscriptionPayload from "./SubscriptionPayload";
-import { hashPassword } from "../password";
-
-@InputType()
-class CreateUserInput implements Partial<User> {
-  @Field()
-  username: string;
-
-  @Field()
-  displayName: string;
-
-  @Field()
-  password: string;
-}
-
-@InputType()
-class UpdateUserInput implements Partial<User> {
-  @Field({ nullable: true })
-  displayName?: string;
-
-  @Field({ nullable: true })
-  password?: string;
-}
-
-@ObjectType({ implements: SubscriptionPayload })
-class UserSubscriptionPayload extends SubscriptionPayload {
-  @Field((type) => User)
-  user: User;
-}
 
 @Resolver(User)
 class UserResolver {
@@ -55,78 +10,6 @@ class UserResolver {
   async users(@Ctx() context: Context) {
     const { prisma } = context;
     return await prisma.user.findMany();
-  }
-
-  @Mutation((returns) => User)
-  async createUser(
-    @Arg("data") data: CreateUserInput,
-    @Ctx() context: Context,
-    @PubSub() pubsub: PubSubEngine
-  ) {
-    const { prisma } = context;
-
-    const passwordHash: string = hashPassword(data.password);
-
-    const user = await prisma.user.create({
-      data: {
-        displayName: data.displayName,
-        username: data.username,
-        passwordHash,
-      },
-    });
-    pubsub.publish("USER", {
-      mutationType: MutationType.CREATED,
-      user,
-    });
-    return user;
-  }
-
-  @Mutation((returns) => User)
-  async updateUser(
-    @Arg("id", (type) => Int) id: number,
-    @Arg("data") data: UpdateUserInput,
-    @Ctx() context: Context,
-    @PubSub() pubsub: PubSubEngine
-  ) {
-    const { prisma } = context;
-
-    const user = await prisma.user.update({
-      where: { id },
-      data: {
-        displayName: data.displayName,
-        passwordHash: data.password ? hashPassword(data.password) : undefined,
-      },
-    });
-
-    pubsub.publish("USER", {
-      mutationType: MutationType.UPDATED,
-      user,
-    });
-    return user;
-  }
-
-  @Mutation((returns) => User)
-  async deleteUser(
-    @Arg("id", (type) => Int) id: number,
-    @Ctx() context: Context,
-    @PubSub() pubsub: PubSubEngine
-  ) {
-    const { prisma } = context;
-
-    const user = await prisma.user.delete({ where: { id } });
-
-    pubsub.publish("USER", {
-      mutationType: MutationType.DELETED,
-      user,
-    });
-    return user;
-  }
-
-  @Subscription({ topics: "USER" })
-  userSubscription(
-    @Root() userSubscriptionPayload: UserSubscriptionPayload
-  ): UserSubscriptionPayload {
-    return userSubscriptionPayload;
   }
 }
 
