0ebd8db0561a81c4d9bea8d8817e4c7d9917f80e
MortalHappiness <b07901069@ntu.edu.tw>
2021-11-12 03:25:27 +0800

    Reinstall typegraphql-prisma back and directly use its relationResolvers

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,15 +1,20 @@
+import AuthResolver from "./AuthResolver";
 import CommentResolver from "./CommentResolver";
+import LabelResolver from "./LabelResolver";
 import OrganizationResolver from "./OrganizationResolver";
 import ProjectResolver from "./ProjectResolver";
 import TaskResolver from "./TaskResolver";
 import UserResolver from "./UserResolver";
-import MutationResolver from "./MutationResolver";
+
+import { relationResolvers } from "@generated/type-graphql";
 
 export const resolvers = [
+  ...relationResolvers,
+  AuthResolver,
   CommentResolver,
+  LabelResolver,
   OrganizationResolver,
   ProjectResolver,
   TaskResolver,
   UserResolver,
-  MutationResolver,
 ] as const;
