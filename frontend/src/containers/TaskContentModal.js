5ad9717a1ec15880ac4b542f2d2efcd1916128d9
MortalHappiness <b07901069@ntu.edu.tw>
2021-11-19 15:39:46 +0800

    TaskModal title mutation

--- frontend/src/containers/TaskContentModal.js
+++ frontend/src/containers/TaskContentModal.js
@@ -1,10 +1,11 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
-import { GET_TASK_QUERY } from "../graphql";
+import { GET_TASK_QUERY, UPDATE_TASK_MUTATION } from "../graphql";
 import Loading from "../components/Loading";
 
 const style = {
@@ -23,22 +24,34 @@ export default function TaskContentModal({ open, onClose, taskId }) {
   const { loading, error, data } = useQuery(GET_TASK_QUERY, {
     variables: { taskId: parseInt(taskId) },
   });
+  const [updateTask] = useMutation(UPDATE_TASK_MUTATION);
 
   if (loading) return <Loading />;
   if (error) return `Error ${error}`;
 
+  const handleTitleChange = async (e) => {
+    console.log(e.target.value);
+    await updateTask({
+      variables: { taskId: parseInt(taskId), data: { title: e.target.value } },
+    });
+  };
+
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
+    <Modal open={open} onClose={onClose}>
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {data.task.title}
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        <TextField
+          id="task-title"
+          fullWidth
+          defaultValue={data.task.title}
+          onBlur={handleTitleChange}
+          onKeyDown={(e) => {
+            // ENTER key
+            if (e.keyCode === 13) {
+              e.target.blur();
+            }
+          }}
+        ></TextField>
+        <Typography id="task-description" sx={{ mt: 2 }}>
           {data.task.content}
         </Typography>
       </Box>
